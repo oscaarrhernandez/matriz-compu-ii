b367f1343bed0f28e2bb19d5e719887f31654403
Oscar Hernandez <66411411+oscaarrhernandez@users.noreply.github.com>
2022-05-19 11:45:05 +0200

    Add files via upload

--- compuii/marco.asm
+++ compuii/marco.asm
@@ -11,58 +11,72 @@ sumaExt: .word 0
 sumaInt: .word 0
 filas: .byte 0
 columnas: .byte 0
-aux: .byte 0
-cont: .byte 0
-salto: .byte 0
+filimp: .byte 0
+colimp: .byte 0
+f: .byte 0
+c: .byte 0
 marco:
 	std sumaExt
 	sta filas
+	clra
 	stb columnas
 	ldy #m
-	ldb #columnas
+	ldb columnas
 	lslb
 	incb
 	incb
 	clra
 	leay d,y; en y tenemos ya la mat[1][1]
-	clrb
 	ldb filas
 	decb
 	decb
-	stb aux
+	stb filimp
+	clrb
 	ldb columnas
 	decb
 	decb
-	stb salto
-	bucle:
-	ldb cont
-	cmpb aux
-	bhs exit
+	stb colimp
 	
-	ldd #0
-	ldd ,y++
-	addd sumaInt
-	std sumaInt
-	
-	ldb salto
-	clra
-	leay d,y
-	clrb	
-	
-	ldd #0
-	ldb cont
-	incb
-	std cont
-	bra bucle
-	
-	
-exit:
-	ldd sumaExt
-	subd sumaInt
-	std sumaExt
-	jsr imprimir_num
-	ldd sumaInt
-	jsr imprimir_num
+	buclefil:
+		ldb f
+		cmpb filimp
+		bge fin_filas
+		ldb #0
+		stb c
+		buclecol:
+		ldb c
+		cmpb colimp
+		bge fin_col
+		ldd #0
+		ldd ,y++
+		addd sumaInt
+		std sumaInt
+		jsr imprimir_num
+		;ldd #0
+		ldb c
+		incb
+		std c
+		bra buclecol
+	fin_col:
+		clra
+		ldb #0
+		stb c
+		ldb f
+		incb 
+		stb f
+		ldd ,y++
+		ldd ,y++
+		clra clrb
+		bra buclefil
+	fin_filas:
+		clra clrb
+		ldd sumaExt
+		subd sumaInt
+		std sumaExt
+		jsr imprimir_num
+		ldd sumaInt
+		jsr imprimir_num
+		
 	
 
 rts 
