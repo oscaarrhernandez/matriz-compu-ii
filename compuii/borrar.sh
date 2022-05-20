3de6882bec38c892d76df08371524909f2e969f3
Oscar Hernandez <66411411+oscaarrhernandez@users.noreply.github.com>
2022-05-20 12:15:23 +0200

    Delete compuii directory

--- compuii/borrar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm *.lst 
-rm *.rel 
-rm *.rst 
-rm *.map
-
-echo ".lst .rel .rst .map borrados"
--- compuii/ejecutar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ensambla main carga_mat marco maximo leer_num leer_dim imprimir_num imprime_cadena
-rm *.lst 
-rm *.rel 
-rm *.rst 
-rm *.map
-
-echo ".lst .rel .rst .map borrados"
